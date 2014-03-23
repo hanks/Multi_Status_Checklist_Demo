40e3da6b0060f46c4a80c2ef7038d51b7ee79506
hanks <zhouhan315@gmail.com>
2014-03-23 22:46:13 +0900

    add basic progress bar

--- ScrollCellTest/CustomTableViewController.m
+++ ScrollCellTest/CustomTableViewController.m
@@ -4,6 +4,11 @@
 @interface CustomTableViewController () <CustomCellTableViewCellDelegate>
 
 @property NSMutableArray *_objects;
+@property (nonatomic, weak)UIView *progressBarView;
+@property (nonatomic, weak)UILabel *percentageLabel;
+@property (nonatomic, weak)UIView *progressBar;
+
+@property int percentageValue;
 
 @end
 
@@ -29,6 +34,36 @@
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
     [self loadInitData];
+    [self initProgressbarView];
+}
+
+- (void)initProgressbarView {
+    UIView *progressBarView = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, CGRectGetWidth(self.tableView.bounds), 20)];
+    self.progressBarView = progressBarView;
+    
+    UILabel *percentageLabel = [[UILabel alloc] initWithFrame:CGRectMake(22.0f, 1.0f, 20.0f, 20.0f)];
+    percentageLabel.text = @"0%";
+    percentageLabel.font = [UIFont fontWithName:@"Helvetica" size:13];
+    percentageLabel.textAlignment = NSTextAlignmentRight;
+    self.percentageLabel = percentageLabel;
+    [self.progressBarView addSubview:percentageLabel];
+    
+    UIView *progressBar = [[UIView alloc] initWithFrame:CGRectMake(49.0f, 3.0f, CGRectGetWidth(self.tableView.bounds) - 51, 15.f)];
+    [progressBar setBackgroundColor:[self colorFromHexString:@"#D9D9D9"]];
+    [progressBar.layer setCornerRadius:6.0f];
+    self.progressBar = progressBar;
+    [self.progressBarView addSubview:self.progressBar];
+    
+    self.tableView.tableHeaderView = self.progressBarView;
+    
+}
+
+- (UIColor *)colorFromHexString:(NSString *)hexString {
+    unsigned rgbValue = 0;
+    NSScanner *scanner = [NSScanner scannerWithString:hexString];
+    [scanner setScanLocation:1]; // bypass '#' character
+    [scanner scanHexInt:&rgbValue];
+    return [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16)/255.0 green:((rgbValue & 0xFF00) >> 8)/255.0 blue:(rgbValue & 0xFF)/255.0 alpha:1.0];
 }
 
 - (void)loadInitData {
